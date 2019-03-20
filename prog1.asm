9e509eb3e66fb98b9a1d2387e41947e6d91c9505
sizu-capsaicin <sizu@inl.ics.keio.ac.jp>
2019-03-20 03:10:37 +0000

    correct prog1.asm

--- prog1.asm
+++ prog1.asm
@@ -6,11 +6,11 @@ section .text
 global _start
 
 _start:
-	mov rax, 0x04			; System call number for write = 4
+	mov rax, 1			; System call number for write = 4
 	mov rdi, 1			; Write to stdout
 	mov rsi, hello_world		; The address to "Hello World"
 	mov rdx, len			; The size of write
 	syscall				; Involke the kernel
-	mov rax, 0x01			; System call number for exit = 1
+	mov rax, 60			; System call number for exit = 1
 	mov rdi, 0			; Exit Success = 0i
 	syscall				; Involke the kernel
