fb71ea280b0d98d5eaeca7ff0f957a5e08b39179
sizu-capsaicin <47233199+sizu-capsaicin@users.noreply.github.com>
2019-05-11 23:18:11 +0900

    Delete .netlink_module.ko.cmd

--- .netlink_module.ko.cmd
+++
@@ -1,1 +0,0 @@
-cmd_/home/sizu/shinjin_git/netlink_module.ko := ld -r -m elf_x86_64 -z max-page-size=0x200000 -T ./scripts/module-common.lds --build-id  -o /home/sizu/shinjin_git/netlink_module.ko /home/sizu/shinjin_git/netlink_module.o /home/sizu/shinjin_git/netlink_module.mod.o ;  true
