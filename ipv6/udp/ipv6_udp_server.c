fc2eccbdf3284d4bd2ac8164cce17ecb81242aff
sizu-capsaicin <sizu@inl.ics.keio.ac.jp>
2019-05-12 12:41:04 +0000

    fix (socklen_t *) cast and compile

--- ipv6/udp/ipv6_udp_server.c
+++ ipv6/udp/ipv6_udp_server.c
@@ -60,7 +60,7 @@ int main(int argc, char *argv[])
 	while (1) {			// execute forever
 		int size;		// size of data
 
-		if ((size = recvfrom(desc, buf, BUF, 0, client->ai_addr, client->ai_addrlen)) < 0) {
+		if ((size = recvfrom(desc, buf, BUF, 0, client->ai_addr, (socklen_t *) &client->ai_addrlen)) < 0) {
 			perror("recv error\n");
 			exit(1);
 		}
